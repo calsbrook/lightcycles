8d410bac4ffdd57cefc92aff61b352efe1ff83af
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-03-05 10:17:52 -0800

    refactor complete but not fully implemented

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Redirect
 } from 'react-router-dom';
 import CanvasComponent from './CanvasComponent'
+import GameComponent from './GameComponent'
 import {Layer, Rect, Stage, Group, Line} from 'react-konva';
 import userService from './utils/userService'
 import NavBar from './NavBar/NavBar'
@@ -14,148 +15,7 @@ import LoginPage from './LoginPage/LoginPage'
 import ProfilePage from './ProfilePage/ProfilePage'
 import './App.css';
 
-var player1 = {
-  x: 550,
-  y: 250,
-  previous: [{x: 550, y: 250}],
-  direction: null,
-  speed: 5
-}
-
-var player2 = {
-  x: 160,
-  y: 250,
-  previous: [{x: 160, y: 250}],
-  direction: null,
-  speed: 5
-}
-
-const canvas = document.getElementById('canvovo')
-const ctx = canvas.getContext("2d");
-document.addEventListener("keydown", keyDownHandler, false);
-
-function draw() {
-  drawBackground();
-  move();
-  drawTrail();
-  drawBike();
-  if (collision()) {
-    window.requestAnimationFrame(draw)
-  }
-}
-
-function collision() {
-  let currPos = {x: player1.x, y: player1.y}
-  let width = 700
-  let height = 500
-  let p1PreviousCopy = [...player1.previous]
-  let p2PreviousCopy = [...player2.previous]
-  p1PreviousCopy[p1PreviousCopy.length-1] = {x: 999999, y: 999999}
-  p2PreviousCopy[p2PreviousCopy.length-1] = {x: 999999, y: 999999}
-  let check = false
-  p1PreviousCopy.forEach(function(e) {
-    if ((e.x === currPos.x && e.y === currPos.y)) {
-      console.log('hit')
-      console.log(player1.previous)
-      check = true
-    }
-  })
-  p2PreviousCopy.forEach(function(e) {
-    if ((e.x === currPos.x && e.y === currPos.y)) {
-      console.log('hit')
-      check = true
-    }
-  })
-  if (currPos.x > width ||
-      currPos.y > height ||
-      currPos.x < 0 ||
-      currPos.y < 0) {
-        console.log('hit');
-        check = true
-  }
-  return check 
-}
-
-
-function keyDownHandler(e) {
-  switch(e.key) {
-    case 'w':
-      if(player1.direction !== 'down') {
-        player1.direction = 'up'
-      }
-      break;
-    case 'a':
-      if(player1.direction !== 'right') {
-        player1.direction = 'left'
-      }
-      break;
-    case 's':
-      if(player1.direction !== 'up') {
-        player1.direction = 'down'
-      }
-      break;
-    case 'd':
-      if(player1.direction !== 'left') {
-        player1.direction = 'right'
-      }
-      break;
-  }
-}
-
-function move() {
-  switch(player1.direction) {
-    case 'up':
-      player1.y = player1.y - player1.speed
-      break;
-    case 'left':
-      player1.x = player1.x - player1.speed
-      break;
-    case 'down':
-      player1.y = player1.y + player1.speed
-      break
-    case 'right':
-      player1.x = player1.x + player1.speed
-      break;
-    }
-  player1.previous.push({x: player1.x, y: player1.y})
-}
-
-function drawTrail() {
-  let trail = parseTrail(player1.previous)
-  ctx.beginPath();
-  ctx.lineWidth = 5
-  ctx.strokeStyle = 'orange'
-  ctx.moveTo(trail[0], trail[1])
-  for (let i = 2; i < trail.length; i += 2) {
-    ctx.lineTo(trail[i], trail[i + 1])
-  }
-  ctx.stroke();
-}
-
-function parseTrail(previous) {
-  let trail = []
-  previous.forEach(function (key) {
-    trail.push(key.x)
-    trail.push(key.y)
-  })
-  return trail
-}
-
-function drawBackground() {
-  ctx.beginPath();
-  ctx.rect(0, 0, 700, 500)
-  ctx.fillStyle = "gray";
-  ctx.fill()
-  ctx.closePath()
-}
-
-function drawBike() {
-  ctx.beginPath();
-  ctx.rect(player1.x-5, player1.y-5, 10, 10)
-  ctx.fillStyle = "orange";
-  ctx.fill()
-  ctx.closePath();
-}
+// react
 
 class App extends Component {
 
@@ -164,7 +24,7 @@ class App extends Component {
     this.state = {
       user: {email: null},
       player1: {
-        playerX: player1.x,
+        playerX: 10,
         playerY: 250,
         color: 'orange',
         previous: [{playerX: 550, playerY: 250}],
@@ -221,32 +81,32 @@ class App extends Component {
   // }
 
   collision = () => {
-    let currPos = {x: player1.x, y: player1.y}
-    let width = this.state.width
-    let height = this.state.height
-    player1.previous[player1.previous.length-1] = {x: 999999, y: 999999}
-    player2.previous[player2.previous.length-1] = {x: 999999, y: 999999}
-    let check = false
-    player1.previous.forEach(function(e) {
-      if ((e.x === currPos.x && e.y === currPos.y)) {
-        console.log('hit')
-        check = true
-      }
-    })
-    player2.previous.forEach(function(e) {
-      if ((e.x === currPos.x && e.y === currPos.y)) {
-        console.log('hit')
-        check = true
-      }
-    })
-    if (currPos.x > width ||
-        currPos.y > height ||
-        currPos.x < 0 ||
-        currPos.y < 0) {
-          console.log('hit');
-          check = true
-    }
-    return check 
+    // let currPos = {x: player1.x, y: player1.y}
+    // let width = this.state.width
+    // let height = this.state.height
+    // player1.previous[player1.previous.length-1] = {x: 999999, y: 999999}
+    // player2.previous[player2.previous.length-1] = {x: 999999, y: 999999}
+    // let check = false
+    // player1.previous.forEach(function(e) {
+    //   if ((e.x === currPos.x && e.y === currPos.y)) {
+    //     console.log('hit')
+    //     check = true
+    //   }
+    // })
+    // player2.previous.forEach(function(e) {
+    //   if ((e.x === currPos.x && e.y === currPos.y)) {
+    //     console.log('hit')
+    //     check = true
+    //   }
+    // })
+    // if (currPos.x > width ||
+    //     currPos.y > height ||
+    //     currPos.x < 0 ||
+    //     currPos.y < 0) {
+    //       console.log('hit');
+    //       check = true
+    // }
+    // return check 
   }
 
   draw = () => {
@@ -285,10 +145,10 @@ class App extends Component {
         //   player2.y = data.y
         //   player2.previous.push({x: data.x, y: data.y})
         // })
-        this.socket.emit('move', {
-          x: this.state.width - player1.x,
-          y: this.state.height - player1.y
-        })
+        // this.socket.emit('move', {
+        //   x: this.state.width - player1.x,
+        //   y: this.state.height - player1.y
+        // })
         // console.log(player1.previous)
         this.setState({
           bullshit: true
@@ -330,14 +190,13 @@ class App extends Component {
       height: 500,
       winner: null
     });
-    this.draw()
   }
 
   componentDidMount() {
     let user = userService.getUser();
     this.setState({user})
-    
-    window.requestAnimationFrame(draw)
+
+    // window.requestAnimationFrame(draw)
   }
 
   componentDidUpdate() {
@@ -385,8 +244,10 @@ class App extends Component {
                 <div>
                   <h1>ライトサイクル</h1>
                   <p>ＬＩＧＨＴ ＣＹＣＬＥＳ</p>
-              {(!this.state.winner) ? (<p>You are {this.state.player1.color}</p>) : (<p>░▒▓ＤＥ － ＲＥＺＥＤ▓▒░</p>)}
+              {(true) ? (<p>You are {this.state.player1.color}</p>) : (<p>░▒▓ＤＥ － ＲＥＺＥＤ▓▒░</p>)}
                   <div className="myGame" >
+                  {/* <canvas ref="game" id="canvovo" width="700" height="500"></canvas> */}
+                  <GameComponent />
                     {/* <Stage tabIndex='1' ref="game" width={this.state.width} height={this.state.height}>
                       <Layer className="board" tabIndex='2'>
                         <Rect 
---
+++ src/GameComponent.js
@@ -0,0 +1,185 @@
+import React, {Component} from 'react'
+
+var gameBoard = {
+    width: 700,
+    height: 500
+}
+
+var player1 = {
+    x: 550,
+    y: 250,
+    previous: [{x: 550, y: 250}],
+    direction: null,
+    speed: 5,
+    lose: false
+  }
+  
+var player2 = {
+    x: 160,
+    y: 250,
+    previous: [{x: 160, y: 250}],
+    direction: null,
+    speed: 5
+}
+
+// const canvas = document.getElementById('canvovo')
+// const ctx = canvas.getContext("2d");
+
+
+class GameComponent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            test: 'test'
+        }
+    }
+
+    draw = () => {
+        this.drawBackground();
+        this.move();
+        this.drawTrail();
+        this.drawBike();
+        if (!this.collision()) {
+            window.requestAnimationFrame(this.draw)
+        }
+    }
+
+    collision() {
+        let currPos = {x: player1.x, y: player1.y}
+        let width = 700
+        let height = 500
+        let p1PreviousCopy = [...player1.previous]
+        let p2PreviousCopy = [...player2.previous]
+        p1PreviousCopy[p1PreviousCopy.length-1] = {x: 999999, y: 999999}
+        p2PreviousCopy[p2PreviousCopy.length-1] = {x: 999999, y: 999999}
+        let check = false
+        p1PreviousCopy.forEach(function(e) {
+            if ((e.x === currPos.x && e.y === currPos.y)) {
+            console.log('hit')
+            console.log(player1.previous)
+            player1.lose = true
+            check = true
+            }
+        })
+        p2PreviousCopy.forEach(function(e) {
+            if ((e.x === currPos.x && e.y === currPos.y)) {
+            console.log('hit')
+            player1.lose = true
+            check = true
+            }
+        })
+        if (currPos.x > width ||
+            currPos.y > height ||
+            currPos.x < 0 ||
+            currPos.y < 0) {
+                console.log('hit');
+                player1.lose = true
+                check = true
+        }
+        return check
+    }
+
+    keyDownHandler(e) {
+        switch(e.key) {
+            case 'w':
+                if(player1.direction !== 'down') {
+                    player1.direction = 'up'
+                }
+                break;
+            case 'a':
+                if(player1.direction !== 'right') {
+                    player1.direction = 'left'
+                }
+                break;
+            case 's':
+                if(player1.direction !== 'up') {
+                    player1.direction = 'down'
+                }
+                break;
+            case 'd':
+                if(player1.direction !== 'left') {
+                    player1.direction = 'right'
+                }
+                break;
+        }
+    }
+
+    move() {
+        if(player1.direction) {
+            switch(player1.direction) {
+            case 'up':
+                player1.y = player1.y - player1.speed
+                break;
+            case 'left':
+                player1.x = player1.x - player1.speed
+                break;
+            case 'down':
+                player1.y = player1.y + player1.speed
+                break
+            case 'right':
+                player1.x = player1.x + player1.speed
+                break;
+            default :
+                break;
+            }
+            player1.previous.push({x: player1.x, y: player1.y})
+        }
+    }
+
+    parseTrail(previous) {
+        let trail = []
+        previous.forEach(function (key) {
+            trail.push(key.x)
+            trail.push(key.y)
+        })
+        return trail
+    }
+
+    drawTrail() {
+        const canvas = this.refs.canvas
+        const ctx = canvas.getContext("2d")
+        let trail = this.parseTrail(player1.previous)
+        ctx.beginPath();
+        ctx.lineWidth = 5
+        ctx.strokeStyle = 'orange'
+        ctx.moveTo(trail[0], trail[1])
+        for (let i = 2; i < trail.length; i += 2) {
+            ctx.lineTo(trail[i], trail[i + 1])
+        }
+        ctx.stroke();
+    }
+
+    drawBike() {
+        const canvas = this.refs.canvas
+        const ctx = canvas.getContext("2d")
+        ctx.beginPath();
+        ctx.rect(player1.x-5, player1.y-5, 10, 10)
+        ctx.fillStyle = "orange";
+        ctx.fill()
+        ctx.closePath();
+    }
+
+    drawBackground = () => {
+        const canvas = this.refs.canvas
+        const ctx = canvas.getContext("2d")
+        ctx.beginPath();
+        ctx.rect(0, 0, 700, 500)
+        ctx.fillStyle = "gray";
+        ctx.fill()
+        ctx.closePath()
+    }
+
+    componentDidMount() {
+        player1.direction = 'left'
+        document.addEventListener('keydown', this.keyDownHandler, false)
+        this.draw();
+    }
+
+    render() {
+        return (
+            <canvas ref="canvas" width={700} height={500}/>
+        )
+    }
+}
+
+export default GameComponent
